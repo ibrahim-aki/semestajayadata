b716e2162594db1bbdfbf856ea43cf0754dd86a7
ibrahim-aki <mabesoperator@gmail.com>
2025-07-20 14:47:24 +0700

    apdt

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,9 @@
 import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// Konfigurasi untuk memastikan Vercel menyuntikkan ENV ke import.meta.env
 export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, process.cwd(), '');
+  const env = loadEnv(mode, process.cwd(), '')
 
   return {
     plugins: [react()],
